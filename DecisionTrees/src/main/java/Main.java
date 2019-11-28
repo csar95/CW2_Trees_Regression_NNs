e45903f05b1286c0cf44d92e26977b11ce48c7ea
Csar Gutirrez Carrero <cg93@hw.ac.uk>
2019-11-28 16:23:58 +0000

    Random Forest multiple tests

--- DecisionTrees/src/main/java/Main.java
+++ DecisionTrees/src/main/java/Main.java
@@ -2,32 +2,36 @@ public class Main {
 
     public  static void main(String[] args)
     {
-        J48 j48 = new J48();
-        String[] settings = {
-                "-C 0.25 -M 2",
-                "-C 0.25 -M 4",
-                "-C 0.25 -B -M 2",
-                "-C 0.25 -B -M 4",
-                "-C 0.5 -M 2",
-                "-C 0.5 -M 4",
-                "-C 0.5 -B -M 2",
-                "-C 0.5 -B -M 4",
-                "-U -M 2",
-                "-U -M 4",
-                "-U -B -M 2",
-                "-U -B -M 4"
-        };
-        for (String s : settings) {
-            for (Datasets d : Datasets.values()) {
-                j48.run(s, d);
-            }
-        }
+//        J48 j48 = new J48();
+//        String[] settings = {
+//                "-C 0.25 -M 2",
+//                "-C 0.25 -M 4",
+//                "-C 0.25 -B -M 2",
+//                "-C 0.25 -B -M 4",
+//                "-C 0.5 -M 2",
+//                "-C 0.5 -M 4",
+//                "-C 0.5 -B -M 2",
+//                "-C 0.5 -B -M 4",
+//                "-U -M 2",
+//                "-U -M 4",
+//                "-U -B -M 2",
+//                "-U -B -M 4"
+//        };
+//        for (String s : settings) {
+//            for (Datasets d : Datasets.values()) {
+//                j48.run(s, d);
+//            }
+//        }
 
 //        j48.run("-C 0.25 -M 2", Datasets.TEST_FULL);
 
-//        RandomForest randomForest = new RandomForest();
-//        String defaultRF = "-P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1";
+        RandomForest randomForest = new RandomForest();
+        String defaultRF = "-P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1";
 //        randomForest.run(defaultRF);
+
+        for (Datasets d : Datasets.values()) {
+            randomForest.run(defaultRF, d);
+        }
 
 //        UserClassifier userClassifier = new UserClassifier();
 //        userClassifier.run();
--- DecisionTrees/src/main/java/RandomForest.java
+++ DecisionTrees/src/main/java/RandomForest.java
@@ -1,38 +1,125 @@
 import weka.classifiers.Evaluation;
+import weka.classifiers.misc.InputMappedClassifier;
 import weka.core.Instances;
 import weka.core.Utils;
 import weka.core.converters.ConverterUtils.DataSource;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Random;
 
 
 public class RandomForest {
 
-    public void run (String options)
+    public void run (String options, Datasets datasets)
     {
         try
         {
-            String path = getClass().getResource("trainingData_top10.csv").getPath();
-            DataSource source = new DataSource(path);
-            Instances data = source.getDataSet();
-            data.setClassIndex(data.numAttributes() - 1);
+            String path_train = "", path_test = "";
+            DataSource source_train = null, source_test = null;
+            Instances data_train = null, data_test = null;
+
+            switch (datasets) {
+                case KFOLD:
+                    path_train = getClass().getResource("trainingData_top10.csv").getPath();
+                    source_train = new DataSource(path_train);
+                    data_train = source_train.getDataSet();
+                    data_train.setClassIndex(data_train.numAttributes() - 1);
+                    break;
+                case TEST_FULL:
+                    path_train = getClass().getResource("trainingData_top10.csv").getPath();
+                    source_train = new DataSource(path_train);
+                    data_train = source_train.getDataSet();
+                    data_train.setClassIndex(data_train.numAttributes() - 1);
+
+                    path_test = getClass().getResource("testingData_top10.csv").getPath();
+                    source_test = new DataSource(path_test);
+                    data_test = source_test.getDataSet();
+                    data_test.setClassIndex(data_test.numAttributes() - 1);
+                    break;
+                case TEST_4000:
+                    path_train = getClass().getResource("trainingData_top10_4000.csv").getPath();
+                    source_train = new DataSource(path_train);
+                    data_train = source_train.getDataSet();
+                    data_train.setClassIndex(data_train.numAttributes() - 1);
+
+                    path_test = getClass().getResource("testingData_top10_4000.csv").getPath();
+                    source_test = new DataSource(path_test);
+                    data_test = source_test.getDataSet();
+                    data_test.setClassIndex(data_test.numAttributes() - 1);
+                    break;
+                case TEST_9000:
+                    path_train = getClass().getResource("trainingData_top10_9000.csv").getPath();
+                    source_train = new DataSource(path_train);
+                    data_train = source_train.getDataSet();
+                    data_train.setClassIndex(data_train.numAttributes() - 1);
+
+                    path_test = getClass().getResource("testingData_top10_9000.csv").getPath();
+                    source_test = new DataSource(path_test);
+                    data_test = source_test.getDataSet();
+                    data_test.setClassIndex(data_test.numAttributes() - 1);
+                    break;
+                default:
+                    throw new Exception("You need to select a compatible data set name (KFOLD, TEST_FULL, TEST_4000, TEST_9000)");
+            }
+
+//            String path = getClass().getResource("trainingData_top10.csv").getPath();
+//            DataSource source = new DataSource(path);
+//            Instances data = source.getDataSet();
+//            data.setClassIndex(data.numAttributes() - 1);
+
+            InputMappedClassifier mappedCls = new InputMappedClassifier();
 
             // Create new instance of scheme
             weka.classifiers.trees.RandomForest model = new weka.classifiers.trees.RandomForest();
             model.setOptions(Utils.splitOptions(options));
-            model.buildClassifier(data);
+            model.buildClassifier(data_train);
 
-            System.out.println("=== Classifier model (full training set) ===\n");
-            System.out.println(model);
+            mappedCls.setModelHeader(data_train);
+            mappedCls.setClassifier(model);
+            mappedCls.setSuppressMappingReport(true);
+
+//            System.out.println("=== Classifier model (full training set) ===\n");
+//            System.out.println(model);
 
             // Eval it with test data based on the test data
-            Evaluation eval = new Evaluation(data);
-            eval.evaluateModel(model, data);
-            eval.crossValidateModel(model, data, 10, new Random(1));
+//            Evaluation eval = new Evaluation(data);
+//            eval.evaluateModel(model, data);
+//            eval.crossValidateModel(model, data, 10, new Random(1));
 
-            System.out.println(eval.toSummaryString("=== Summary ===\n", false));
-            System.out.println(eval.toClassDetailsString());
-            System.out.println(eval.toMatrixString("=== Confusion Matrix ===\n"));
+//            System.out.println(eval.toSummaryString("=== Summary ===\n", false));
+//            System.out.println(eval.toClassDetailsString());
+//            System.out.println(eval.toMatrixString("=== Confusion Matrix ===\n"));
+
+            Evaluation eval = null;
+
+            switch (datasets) {
+                case KFOLD:
+                    // Eval it with test data based on the test data
+                    eval = new Evaluation(data_train);
+                    eval.evaluateModel(model, data_train);
+                    eval.crossValidateModel(model, data_train, 10, new Random(1));
+                    break;
+                case TEST_FULL:
+                case TEST_9000:
+                case TEST_4000:
+                    eval = new Evaluation(data_test);
+                    eval.evaluateModel(mappedCls, data_test);
+                    break;
+                default:
+                    throw new Exception("You need to select a compatible data set name (KFOLD, TEST_FULL, TEST_4000, TEST_9000)");
+            }
+
+            String output =
+                    eval.toSummaryString("=== Summary ===\n", false) + "\n" +
+                    eval.toClassDetailsString() + "\n" +
+                    eval.toMatrixString("=== Confusion Matrix ===\n") + "\n" +
+                    "=== Classifier model (full training set) ===\n" + "\n" +
+                    model;
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(getClass().getResource("/").getPath() + "RF_" + datasets.toString() + ".txt"));
+            writer.write(output);
+            writer.close();
         }
         catch (Exception e)
         {
